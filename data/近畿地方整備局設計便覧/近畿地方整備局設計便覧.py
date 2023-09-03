4bf75c84c92a18814045a2db007256fdd180871a
dicechick373 <dicechick373@gmail.com>
2023-09-03 13:19:05 +0900

    test

--- data/近畿地方整備局設計便覧/近畿地方整備局設計便覧.py
+++ data/近畿地方整備局設計便覧/近畿地方整備局設計便覧.py
@@ -1,81 +1,138 @@
 
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.layout import LAParams, LTTextContainer
-from pdfminer.converter import PDFPageAggregator
+from langchain.llms import OpenAIChat
+import streamlit as st
+import os
+import json
+import glob
+from langchain.document_loaders import PyMuPDFLoader
 
 
-# PDFのレイアウト分析
-def pdfminer_layout_analysis(layouts):
-    # 結果を格納する辞書
-    result = {
-        'contents':[],
-        'page':'',
-    }
-    
-    for layout in layouts:
-        # 罫線などのオブジェクトが含まれているとエラーになるのを防ぐため
-        if isinstance(layout, LTTextContainer):
-            # 座標は各ページの左下を原点としている
-            paragraph = {
-                'text':layout.get_text().strip(),
-                'x0':round(layout.x0,2),# x0: テキストの左端のx座標
-                'x1':round(layout.x1,2),# x1: テキストの右端のx座標
-                'y0':round(layout.y0,2),# y0: テキストの下端のy座標
-                'y1':round(layout.y1,2),# y1: テキストの上端のy座標
-                'width':round(layout.width,2),# width: テキストの幅(x1 - x0)
-                'height':round(layout.height,2),# height: テキストの高さ(y1 - y0)
-            }
-                        
-            # ページ番号を格納する（y座標<48で判別できる）
-            if layout.y1 < 48:
-                # print('page number'+layout.get_text().strip())
-                result['page'] = layout.get_text().strip()
-                        
-            # 出典は除外する（x座標>467で判別できる）
-            if layout.x0 > 467:
-                # print('source'+layout.get_text().strip())
-                continue
-            
-            # 結果を格納
-            result['contents'].append(paragraph)
+# utilsのimport
+import sys
+sys.path.append('D:/GoogleDrive/マイドライブ/github_repositories/chat-with-PDFs')
+from utils import PDFMiner
+# sys.path.append('PATH_TO_TENSORFLOW_OBJECT_DETECTION_FOLDER')
+
+# OpenAIを利用するためにプロキシ設定が必要
+os.environ['http_proxy'] = st.secrets["proxy"]["URL"]
+os.environ['https_proxy'] = st.secrets["proxy"]["URL"]
+
+llm = OpenAIChat(openai_api_key=st.secrets["api_keys"]["OPEN_API_KEY"],temperature=0.0)
+
+def format_text(page_content):
+    prompt = f'''
+        以下に示すデータは、PDF形式の文書をPDFyMuPDFLoaderによりテキスト化したものである。
+        このデータから、下記のルールに従ってJSONデータに変換せよ。
+        JSONデータが途中で切れている場合は、その部分を削除して、正規のJSONデータに変換せよ。
+
+        ルール:
+        - JSONのキーは'text'とする。
+        - 全角英数字は、半角英数字に変換する。
+        - 文字間の空白は、全て削除する。
+        - 二重引用符（"）、バックスラッシュ（\）、改行文字（\n）、タブ文字（\t）などの特殊文字が含まれる場合は、エスケープ文字を付与する。
         
-    return result
-
-
-def pdfminer(path):
-    manager = PDFResourceManager()
-
-    result = []
-
-    with open(path, 'rb') as input:
-        with PDFPageAggregator(manager, laparams=LAParams()) as device:
-            # PDFPageInterpreterオブジェクトの取得
-            iprtr = PDFPageInterpreter(manager, device)
-            # ページごとで処理を実行
-            for page in PDFPage.get_pages(input):
-                iprtr.process_page(page)
-                # ページ内の各テキストのレイアウト
-                layouts = device.get_result()
-
-                # レイアウト分析結果
-                contents = pdfminer_layout_analysis(layouts)
-                result.append(contents)
+        # データ
+        {page_content}]
+        '''
     
-    return result
+    return llm(prompt)
+
+# def format_metadata(page_content):
+#     prompt = f'''
+#         以下に示すデータは、PDF形式の文書をPDFyMuPDFLoaderによりテキスト化したものである。
+#         このデータから、下記のルールに従ってページ番号をJSONデータに変換せよ。
+
+#         ルール:
+#         - 最下部のテキストをページ番号とする。改行コードは削除する。
+#         - JSONのキーは'page_number'とする。
+        
+#         # データ
+#         {page_content}]
+#         '''
+#     return llm(prompt)
+
+def format_table(page_content):
+    prompt = f'''
+        以下に示すデータは、PDF形式の文書をPDFyMuPDFLoaderによりテキスト化したものである。
+        このデータから、表形式として認識できる情報を全て、下記のルールに従ってJSONデータに変換せよ。
+        表形式の情報が複数ある場合は、全ての情報をJSONデータに変換せよ。
+
+        ルール:
+        - JSONのキーは'table'とする。
+        - '表'を含むテキストの下部に、表形式の情報があることが多い。   
+        - 表のタイトルは、表の上にある文章として認識する。JSONのキーは'title'とする。
+        - 表の内容は、表形式のjsonに変換する。JSONのキーは'data'とする。
+        - 二重引用符（"）、バックスラッシュ（\）、改行文字（\n）、タブ文字（\t）などの特殊文字が含まれる場合は、エスケープ文字を付与する。
+        
+        # データ
+        {page_content}]
+        '''
+    
+    return llm(prompt)
+
+
+def format_dict(data):
+    content = data.page_content.replace("（", "(") \
+    .replace("）", ")") \
+    .replace("－", "-") \
+    .replace("：", ":") \
+    .replace("［", "[") \
+    .replace("］", "]")
+
+    print(content)
+    print(format_text(content))
+    text = json.loads(format_text(content))['text']
+    print(text)
+    table = json.loads(format_table(content))['table']
+    print(table)
+
+    return {
+        "text": text,
+        "table": table,
+    }
+
 
 
 def main():
-    pdf_path = 'data\近畿地方整備局設計便覧\テスト.pdf'
-    result = pdfminer(pdf_path)
+    pdf_path = 'data\近畿地方整備局設計便覧\PDF_READ\土木工事共通編.pdf'
+
+    # pdfminerのレイアウト分析
+    pdfminer = PDFMiner.PDFTextAnalyzer(pdf_path)
+    top_of_y_coord = 48 # ページ番号を取得するためのしきい値（y座標）を設定
+    pages = pdfminer.get_page_number(top_of_y_coord)
+    # print(len(pages)) # 総ページ数の確認　
 
     from langchain.document_loaders import PDFMinerLoader
     loader = PDFMinerLoader(pdf_path)
     data= loader.load_and_split()
+
+    # test = data[16]
+    print(format_dict(data[16]))
+   
+    # for i, d in enumerate(data):
+
+    #     # ページ番号を取得
+    #     page = pages[i]
+
+    #     # 保存先の設定
+    #     dir_path= f'data/近畿地方整備局設計便覧/JSON/土木工事共通編'
+    #     os.makedirs(dir_path, exist_ok=True)
+
+    #     # すでに存在する場合はスキップ
+    #     json_path = f'{dir_path}/土木工事共通編_{page}.json'
+    #     if os.path.isfile(json_path):
+    #         print(f'{page}はすでに存在します。')
+    #         continue
+        
+    #     # データの整形
+    #     dict_data = format_dict(d)
+    #     dict_data["page_number"] = page
+
+    #     # JSONに保存
+    #     with open(f'{dir_path}/土木工事共通編_{page}.json', 'w',  encoding='utf-8') as f:
+    #         json.dump(dict_data, f, indent=2, ensure_ascii=False)
+    #         print(f'{page}を保存しました。')
+
     
-    print(data[0])
-
-
-
 if __name__ == '__main__':
     main()
---
+++ utils/PDFMiner.py
@@ -0,0 +1,94 @@
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams, LTTextContainer
+from pdfminer.converter import PDFPageAggregator
+
+class PDFTextAnalyzer:
+    """
+    PDF文書内のテキストを分析するクラス。PDFMinerを使用。
+
+    Args:
+        pdf_path (str): 分析対象のPDF文書のパス。
+
+    Attributes:
+        pdf_path (str): PDF文書のパス。
+    
+    Methods:
+        get_page_number(self, top_of_y_coord):
+            指定したY座標を基準にPDF文書からページ番号を抽出。
+    """
+
+    def __init__(self, pdf_path):
+        """
+        PDFTextAnalyzerインスタンスの初期化。
+
+        Args:
+            pdf_path (str): 分析対象のPDF文書のパス。
+        """
+        self.pdf_path = pdf_path
+
+    def get_page_number(self, top_of_y_coord):
+        """
+        指定したY座標を基準にPDF文書からページ番号を抽出。
+
+        Args:
+            top_of_y_coord (int): ページ番号抽出のY座標の閾値。
+
+        Returns:
+            str: 抽出されたページ番号（見つからない場合は空文字列）。
+        """
+        return _page_numbers(self.pdf_path, top_of_y_coord)
+        
+        
+def _page_numbers(pdf_path,top_of_y_coord):
+    manager = PDFResourceManager()
+
+    result = []
+    with open(pdf_path, 'rb') as input:
+        with PDFPageAggregator(manager, laparams=LAParams()) as device:
+            # PDFPageInterpreterオブジェクトの取得
+            iprtr = PDFPageInterpreter(manager, device)
+            # ページごとで処理を実行
+            for page in PDFPage.get_pages(input):
+                iprtr.process_page(page)
+                # ページ内の各テキストのレイアウト
+                layouts = device.get_result()
+
+                page_number = _analysis_page_number(layouts,top_of_y_coord)
+                result.append(page_number)
+    return result
+
+def _analysis_page_number(layouts,top_of_y_coord):
+    for layout in layouts:
+        if isinstance(layout, LTTextContainer):
+            if layout.y1 < top_of_y_coord:
+                return layout.get_text().strip()
+                        
+    return ''
+
+
+def pdfminer_layout_analysis(layouts):
+    
+    page_number = ''
+
+    for layout in layouts:
+        # 罫線などのオブジェクトが含まれているとエラーになるのを防ぐため
+        if isinstance(layout, LTTextContainer):
+            # 座標は各ページの左下を原点としている
+            # paragraph = {
+            #     'text':layout.get_text().strip(),
+            #     'x0':round(layout.x0,2),# x0: テキストの左端のx座標
+            #     'x1':round(layout.x1,2),# x1: テキストの右端のx座標
+            #     'y0':round(layout.y0,2),# y0: テキストの下端のy座標
+            #     'y1':round(layout.y1,2),# y1: テキストの上端のy座標
+            #     'width':round(layout.width,2),# width: テキストの幅(x1 - x0)
+            #     'height':round(layout.height,2),# height: テキストの高さ(y1 - y0)
+            # }
+            # print(paragraph)
+                        
+            # ページ番号を格納する（y座標<48で判別できる）
+            if layout.y1 < 48:
+                page_number = layout.get_text().strip()
+                        
+    return page_number
